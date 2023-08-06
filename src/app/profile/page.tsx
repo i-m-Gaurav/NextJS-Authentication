23690a7b0c0688c02a3025ec7364db788683b491
Gaurav Kumar <indiagauravkumar@gmail.com>
2023-08-06 18:55:55 +0530

    added the middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import axios from "axios";
+import React,{useState} from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 
 export default function Profile(){
 
     const router = useRouter()
-
+    const [data,setData] = useState("nothing");
     const logout = async () =>{
 
         console.log("hello")
@@ -24,15 +27,26 @@ export default function Profile(){
         }
     }
 
+    const getUserDetails = async () =>{
+        const response = await axios.get('/api/users/me')
+        console.log(response.data);
+        setData(response.data.data.username);
+    }
+
     return(
         <>
              <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <h1>Profile page</h1>
-                
+                <h2>{data === 'nothing' ? "Nothing": <Link href={`/profile/${data}`}>{data}</Link>}</h2>
                 <button
                 onClick={logout}
                 className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >LogOut</button>
+                 <button
+                onClick={getUserDetails}
+                className="bg-blue-700 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >getUser Data</button>
+
              </div>
         </>
     )
